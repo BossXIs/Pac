62ea71c5c231c43f391efd9aff327e7e03f898d3
BossXI <bossweerawut@hotmail.com>
2022-01-28 16:42:23 +0700

    first commit

--- Source/Headers/ConvertSketch.hpp
+++ Source/Headers/ConvertSketch.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
-std::array<std::array<Cell, MAP_HEIGHT>, MAP_WIDTH> convert_sketch(const std::array<std::string, MAP_HEIGHT>& i_map_sketch, std::array<Position, 4>& i_ghost_positions, Pacman& i_pacman);
+using namespace std;
+
+array<array<Cell, MAP_HEIGHT>, MAP_WIDTH> convert_sketch(const std::array<std::string, MAP_HEIGHT>& i_map_sketch, std::array<Position, 4>& i_ghost_positions, Pacman& i_pacman);
